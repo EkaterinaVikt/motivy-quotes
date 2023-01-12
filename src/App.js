7d35ab2b674e823f3906b080bb16d0e0893845ad
EkaterinaVikt <kat.abramova@gmail.com>
2023-01-12 16:03:55 +0300

    Всё работает, за исключением сброса

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import MainPage from "./Components/MainPage";
+// import MainPageUseEff from "./Components/MainPageUseEff";
 
 function App(props) {
+  // return <MainPage store={props.store} />;
+
   return <MainPage store={props.store} />;
 }
 
--- src/Components/Alarm/Alarm.jsx
+++ src/Components/Alarm/Alarm.jsx
@@ -1,11 +1,10 @@
 import React from "react";
+import styles from "./../MainPage.module.css";
 
 export default function Alarm({ showAlarm }) {
   return (
-    <div>
-      <div className={showAlarm ? styles.showAlarm : styles.nonShowAlarm}>
-        Свежая мысль появится завтра. А сейчас пора за работу!:)
-      </div>
+    <div className={showAlarm ? styles.showAlarm : styles.nonShowAlarm}>
+      Свежая мысль появится завтра. А сейчас пора за работу!:)
     </div>
   );
 }
